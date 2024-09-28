7fb5bd632d8ff90d522d0387f41d500876568c28
PhotonatedQubit <11320427+DebjyotiPaul@users.noreply.github.com>
2024-09-28 17:02:43 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,63 +1,82 @@
 import sys
-
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
-
-def match_pattern(input_line, pattern):
-    if not pattern:
+def matcher(input_line, pattern):
+    ptr1 = 0
+    ptr2 = 0
+    if input_line == "" and pattern == "":
         return True
-    if not input_line:
+    elif input_line == "":
         return False
-    #if pattern[0] == input_line[0]:
-    if pattern[0] == "^":
-        return match_pattern(input_line, pattern[1:])
-    #elif pattern[-1] == "$" :
-        #return input_line == "" and match_pattern(input_line, pattern[:-1])
+    elif pattern == "":
+        return True
+    while ptr1 < len(input_line):
+        if ptr2 + 1 < len(pattern) and pattern[ptr2 : ptr2 + 2] == "\\d":
+            if input_line[ptr1].isdigit():
+                return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 2 :])
+            else:
+                ptr1 = ptr1 + 1
+        elif ptr2 + 1 < len(pattern) and pattern[ptr2 : ptr2 + 2] == "\\w":
+            if input_line[ptr1].isalnum():
+                return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 2 :])
+            else:
+                ptr1 = ptr1 + 1
+        elif input_line[ptr1] == pattern[ptr2]:
+            if ptr2 + 1 < len(pattern) and pattern[ptr2 + 1] == "+":
+                while (
+                    ptr1 + 1 < len(input_line)
+                    and input_line[ptr1 + 1] == input_line[ptr1]
+                ):
+                    ptr1 = ptr1 + 1
+                ptr2 = ptr2 + 1
+            return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 1 :])
+        else:
+            ptr1 = ptr1 + 1
+    return False
+def match_pattern(input_line, pattern):
+    if len(pattern) == 1:
+        return pattern in input_line
+    elif pattern == "\\d":
+        for i in range(10):
+            if input_line.find(str(i)) != -1:
+                return True
+    elif pattern == "\\w":
+        return input_line.isalnum()
+    elif pattern[0:2] == "[^":
+        pat = pattern[2:-1]
+        for ch in pat:
+            if input_line.find(ch) != -1:
+                return False
+        return True
+    elif pattern[0] == "[" and pattern[-1] == "]":
+        pat = pattern[1:-1]
+        for ch in pat:
+            if input_line.find(ch) != -1:
+                return True
+        return False
+    elif pattern[0] == "^":
+        if input_line.startswith(pattern[1:]):
+            return True
+        return False
     elif pattern[-1] == "$":
         l = len(pattern[:-1])
         if input_line[-l:] == pattern[:-1]:
             return True
-        
-
-    elif pattern[0] == input_line[0]:
-        return match_pattern(input_line[1:], pattern[1:])
-    elif pattern[:2] == "\d":
-        return (
-            input_line[0].isdigit() and match_pattern(input_line[1:], pattern[2:])
-        ) or match_pattern(input_line[1:], pattern)
-    elif pattern[:2] == "\w":
-        return (
-            (input_line[0].isalnum() or input_line[0] == "_")
-            and match_pattern(input_line[1:], pattern[2:])
-        ) or match_pattern(input_line[1:], pattern)
-    elif pattern[0] == "[":
-        negate = False
-        if pattern[1] == "^":
-            negate = True
-        idx = pattern.find("]")
-        if idx == -1:
-            return False
-        chars = pattern[1:idx]
-        if negate:
-            chars = pattern[2:idx]
-        for c in chars:
-            if c in input_line:
-                return True if (not negate) else False
-        return False if (not negate) else True
-    else:
         return False
-
+    else:
+        return matcher(input_line, pattern)
 def main():
     pattern = sys.argv[2]
     input_line = sys.stdin.read()
-    input_line = input_line.strip()
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+    # Uncomment this block to pass the first stage
     if match_pattern(input_line, pattern):
         exit(0)
     else:
         exit(1)
-
 if __name__ == "__main__":
     main()
