3ccf0e10e7b6a47c1bca6ce8b70ac017fa6a3929
PhotonatedQubit <11320427+DebjyotiPaul@users.noreply.github.com>
2024-09-28 13:42:15 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,20 +4,56 @@ import sys
 # import lark - available if you need it!
 
 
+def matcher(input_line, pattern):
+    ptr1 = 0
+    ptr2 = 0
+    if input_line == "" and pattern == "":
+        return True
+    elif input_line == "":
+        return False
+    elif pattern == "":
+        return True
+    while ptr1 < len(input_line):
+        if ptr2 + 1 < len(pattern) and pattern[ptr2 : ptr2 + 2] == "\\d":
+            if input_line[ptr1].isdigit():
+                return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 2 :])
+            else:
+                ptr1 = ptr1 + 1
+        elif ptr2 + 1 < len(pattern) and pattern[ptr2 : ptr2 + 2] == "\\w":
+            if input_line[ptr1].isalnum():
+                return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 2 :])
+            else:
+                ptr1 = ptr1 + 1
+        elif input_line[ptr1] == pattern[ptr2]:
+            return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 1 :])
+        else:
+            ptr1 = ptr1 + 1
+    return False
+    
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
     elif pattern == "\\d":
-        return any(c.isdigit() for c in input_line)
+        for i in range(10):
+            if input_line.find(str(i)) != -1:
+                return True
     elif pattern == "\\w":
-        return any(c.isalnum() for c in input_line)
-    elif pattern[0]=="[" and pattern[-1]=="]" :
-        if pattern[1]=="^" :
-            return not any(c in pattern[2:-1] for c in input_line)
-        return any(c in pattern[1:-1] for c in input_line)
-        
+        return input_line.isalnum()
+    elif pattern[0:2] == "[^":
+        pat = pattern[2:-1]
+        for ch in pat:
+            if input_line.find(ch) != -1:
+                return False
+        return True
+    elif pattern[0] == "[" and pattern[-1] == "]":
+        pat = pattern[1:-1]
+        for ch in pat:
+            if input_line.find(ch) != -1:
+                return True
+        return False
     else:
-        raise RuntimeError(f"Unhandled pattern: {pattern}")
+        #raise RuntimeError(f"Unhandled pattern: {pattern}")
+        return matcher(input_line, pattern)
 
 
 def main():
